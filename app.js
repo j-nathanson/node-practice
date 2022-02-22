f5852135e0ed7e3bf21590f12a5b270b4b30dad3
jacobnathanson <jacobnath93@gmail.com>
2022-02-22 11:26:21 -0500

    Node Callbacks and Error Handling

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+// import the function from the rectangle file
 const rect = require('./rectangle');
 
 function solveRect(l, w) {
+    // console log inputs
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-    if (l <= 0 || w <= 0) {
-        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-    } else {
-        console.log(`Area of rectangle: ${rect.area(l, w)}`);
-        console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-    }
+    // first validate inputs greater than 0,
+    //  if it fails the callback will have just error argument which is truthy so it will console log the error object's msaage
+    //  if it passes then it will wait 2 seconds and then the callback function will have 'null' for error argument and an object 'rectangle' which has 2 methods
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message);
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This statement is logged after the call to rect()');
 }
+
+// setTimeout is not called for invalid inputs
 
 solveRect(2, 4);
 solveRect(3, 5);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,19 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+// exports shorthand ok for just methods on an export
+// function as a value for entire export so need module.exports
+
+// async function
+module.exports = (x, y, callback) => {
+    // if inputs are invalid create error message and pass it into callback function
+    if (x <= 0 || y <= 0) {
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+    } else {
+        // else pass a 'null' errors argument and also pass and object with key value methods
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            }),
+            2000
+        );
+    }
+};
